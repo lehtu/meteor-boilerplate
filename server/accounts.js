492f6dd988cc6d9bfc732d0d7c411f8cfa85f870
lehtu <lehtu05@gmail.com>
2016-01-17 21:40:38 +0200

    Added config for accounts

--- server/accounts.js
+++ server/accounts.js
@@ -1,3 +1,12 @@
+// See more about this in here: http://docs.meteor.com/#/full/accounts_config
+Accounts.config({
+    sendVerificationEmail: false,
+    forbidClientAccountCreation: false,
+    //restrictCreationByEamilDomain: [String or Function],
+    loginExpirationInDays: 30,
+    //oauthSecretKey: [String]
+});
+
 Accounts.validateNewUser(function (user) {
     if (user.username && user.username.length >= 3)
         return true;
