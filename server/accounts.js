9d4f994af3160ed96ed1c81ec3c234f474cd4480
lehtu <lehtu05@gmail.com>
2016-01-17 20:15:15 +0200

    Added serverside validation for creating new user

---
+++ server/accounts.js
@@ -0,0 +1,5 @@
+Accounts.validateNewUser(function (user) {
+    if (user.username && user.username.length >= 3)
+        return true;
+    throw new Meteor.Error(403, "Username must have at least 3 characters");
+});
