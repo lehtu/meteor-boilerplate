4f7ed66c02a7fe4c757a226170ae88ea32417efc
lehtu <lehtu05@gmail.com>
2016-01-17 19:03:03 +0200

    Added helpers for login & register

---
+++ client/views/login/login.js
@@ -0,0 +1,8 @@
+Template.login.events({
+    'submit form': function(event) {
+        event.preventDefault();
+        var emailVar = event.target.loginEmail.value;
+        var passwordVar = event.target.loginPassword.value;
+        console.log("Form submitted.");
+    }
+});
