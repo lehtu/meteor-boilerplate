e60b728944ffc8621a4a7c91cb385a5f8bf9c8dd
lehtu <lehtu05@gmail.com>
2016-01-22 23:15:39 +0200

    Update signup to use bootstrap

--- client/views/signup/signup.js
+++ client/views/signup/signup.js
@@ -1,8 +1,8 @@
 Template.signup.events({
     'submit form': function(event) {
         event.preventDefault();
-        var emailVar = event.target.registerEmail.value;
-        var passwordVar = event.target.registerPassword.value;
+        var emailVar = event.target.email.value;
+        var passwordVar = event.target.password.value;
         Accounts.createUser({
             email: emailVar,
             password: passwordVar
