f1e813fdb5c84dfbef1ea9fd5796f81225ef8fa5
lehtu <lehtu05@gmail.com>
2016-01-23 01:22:48 +0200

    Add logout functionality

--- client/views/header/header.js
+++ client/views/header/header.js
@@ -2,4 +2,14 @@ Template.header.helpers({
 });
 
 Template.header.events({
+    'click .logout': function() {
+        Meteor.logout(function(error) {
+            if ( error ) {
+                console.log( error.reason, 'warning' );
+            } else {
+                console.log( 'Logged out!', 'success' );
+                FlowRouter.go('home');
+            }
+        });
+    }
 });
