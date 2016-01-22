4909e96cde140bc0e1cef4a70e2d3765c19c5397
lehtu <lehtu05@gmail.com>
2016-01-22 23:02:35 +0200

    Update login view for bootstrap

--- client/views/login/login.js
+++ client/views/login/login.js
@@ -1,8 +1,8 @@
 Template.login.events({
     'submit form': function(event) {
         event.preventDefault();
-        var emailVar = event.target.loginEmail.value;
-        var passwordVar = event.target.loginPassword.value;
+        var emailVar = event.target.email.value;
+        var passwordVar = event.target.password.value;
         Meteor.loginWithPassword(emailVar, passwordVar, function(error){
             if (error) {
                 console.log(error);
