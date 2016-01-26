105582b37142020fcf453490b3652835041bbde8
lehtu <lehtu05@gmail.com>
2016-01-26 22:58:29 +0200

    Add default template helpers for routing

---
+++ client/layouts/default.js
@@ -0,0 +1,28 @@
+var handleRedirect = function(routes, redirect) {
+    var currentRoute = FlowRouter.getRouteName();
+    if (routes.indexOf(currentRoute) > -1) {
+        FlowRouter.go(redirect);
+        return true;
+    }
+};
+
+Template.defaultLayout.helpers({
+    loggingIn: function() {
+        return Meteor.loggingIn();
+    },
+    authenticated: function() {
+        return !Meteor.loggingIn() && Meteor.user();
+    },
+    redirectAuthenticated: function() {
+        return handleRedirect([
+            'login',
+            'signup'
+        ], '/' );
+    },
+    redirectPublic: function() {
+        return handleRedirect([
+            'index',
+            'dashboard'
+        ], '/login' );
+    }
+});
